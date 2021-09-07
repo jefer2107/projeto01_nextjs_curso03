d76163df855c7e8aa71a4c4578b2368d13c4b0b1
jefferson dias lopes <jefer.ld@hotmail.com>
2021-09-06 22:32:15 -0300

    Buscando dados de api utilizando o getServerSide

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,44 @@
+import { GetServerSideProps } from "next"
+import { useEffect, useState } from "react"
 
-export default function Posts(){
+interface Posts{
+    id: string
+    title: string
+}
+
+interface HomeProps{
+    posts: Posts[]
+}
+
+export default function Posts({posts}: HomeProps){
+    // const [posts, setPosts] = useState<Posts[]>([])
+
+    // useEffect(()=>{
+    //     fetch("http://localhost:3333/posts")
+    //         .then(response => response.json())
+    //         .then(data => setPosts(data))
+    // },[])
+
     return(
-        <h1>Posts</h1>
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}> {post.title} </li>
+                ))}
+            </ul>
+        </div>
+        
     )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+    const response = await fetch("http://localhost:3333/posts")
+    const posts = await response.json()
+
+    return{
+        props:{
+            posts
+        }
+    }
 }
